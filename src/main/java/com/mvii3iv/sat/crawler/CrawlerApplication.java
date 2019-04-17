4922d84453620b097e64cfc513388b36c7d15203
Erick Dominguez <erick.dominguez@autozone.com>
2019-04-17 16:54:56 -0600

    ready to generate war

--- src/main/java/com/mvii3iv/sat/crawler/CrawlerApplication.java
+++ src/main/java/com/mvii3iv/sat/crawler/CrawlerApplication.java
@@ -6,13 +6,20 @@ import com.mvii3iv.sat.crawler.components.captcha.CaptchaService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.support.SpringBootServletInitializer;
 import org.springframework.core.env.Environment;
 
 @SpringBootApplication
-public class CrawlerApplication {
+public class CrawlerApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(CrawlerApplication.class, args);
+	}
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(CrawlerApplication.class);
 	}
 
 }
